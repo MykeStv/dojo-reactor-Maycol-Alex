147f4920ea1cb14bb89d51953f7469d8d2103b62
MykeStv <mykestv22@gmail.com>
2022-03-22 10:49:55 -0500

    Correos no repetidos

--- src/main/java/com/sofkau/dojoreactor/DojoReactorApplication.java
+++ src/main/java/com/sofkau/dojoreactor/DojoReactorApplication.java
@@ -1,13 +1,30 @@
 package com.sofkau.dojoreactor;
 
+import com.sofkau.dojoreactor.service.Email;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DojoReactorApplication {
+public class DojoReactorApplication implements CommandLineRunner {
+
+	private static final Logger log = LoggerFactory.getLogger(DojoReactorApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DojoReactorApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		Email email = new Email();
+
+		//CORREOS NO REPETIDOS
+		email.getEmailsNoRepeated().subscribe(e -> log.info("Emails no repetido: " + e));
+		email.countEmailsRepeated().subscribe(e -> log.info("Cantidad de emails no repetidos: " + e));
+
+		
+	}
 }
--- src/main/java/com/sofkau/dojoreactor/service/Email.java
+++ src/main/java/com/sofkau/dojoreactor/service/Email.java
@@ -1,5 +1,8 @@
 package com.sofkau.dojoreactor.service;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,15 +10,27 @@ public class Email {
 
     public List<String> getEmails() {
 
-        List<String> emails = List.of("juan@hotmail.com", "arturo@outlook.com", "juan@outlook.com", "armillia@sofka.com", "grexia@gmail.com",
-                "lorenzo@sofka.com", "albert@gmail.com", "dante@gmail.com", "wendy@gmail.com", "dennis@hotmail.com",
-                "camila@outlook.com", "diane@hotmail.com", "camila@gmail.com", "staicy@hotmail.com", "talia@gmail.com",
+        List<String> emails = List.of("juan@hotmail.com", "arturo@outlook.com", "juan@outlook.com", "armillia@sofka.com", "cirilla@gmail.com",
+                "jinx@sofka.com", "albert@gmail.com", "dante@gmail.com", "wendy@gmail.com", "dennis@hotmail.com",
+                "camila@outlook.com", "diane@hotmail.com", "camila@gmail.com", "staicy@hotmail.com", "dante@gmail.com",
                 "jinx@sofka.com", "cirilla@gmail.com", "juan@gmail.com", "legend2123@gmail.com", "jaskier@sofka.com",
                 "fenix@outlook.com", "lenin@hotmail.com", "justsad2@gmail.com", "armillia@gmail.com", "yennefer@gmail.com",
-                "dante@gmail.com", "geralt@outlook.com", "keta@gmail.com", "cleopatra@sofka.com", "lilly@sofka.com");
+                "dante@gmail.com", "geralt@outlook.com", "justsad2@gmail.com", "cleopatra@sofka.com", "lilly@sofka.com");
 
 
         return emails;
+    }
+
+    //Correos repetidos
+    public Flux<String> getEmailsNoRepeated(){
+        return Flux.fromIterable(getEmails())
+                .distinct();
+    }
+
+    public Mono<Long> countEmailsRepeated() {
+        return Flux.fromIterable(getEmails())
+                .distinct()
+                .count();
     }
 
 }
