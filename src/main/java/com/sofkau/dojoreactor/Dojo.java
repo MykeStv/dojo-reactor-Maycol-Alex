0007c5e2494f2b4b7fe99a424c8ef9c2d0663859
MykeStv <mykestv22@gmail.com>
2022-03-22 10:38:33 -0500

    Creacion de correos

---
+++ src/main/java/com/sofkau/dojoreactor/Dojo.java
@@ -0,0 +1,13 @@
+package com.sofkau.dojoreactor;
+
+import com.sofkau.dojoreactor.service.Email;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dojo {
+
+
+
+
+}
--- src/main/java/com/sofkau/dojoreactor/service/Email.java
+++ src/main/java/com/sofkau/dojoreactor/service/Email.java
@@ -5,8 +5,17 @@ import java.util.List;
 
 public class Email {
 
-    List<String> emails = new ArrayList<>();
+    public List<String> getEmails() {
+
+        List<String> emails = List.of("juan@hotmail.com", "arturo@outlook.com", "juan@outlook.com", "armillia@sofka.com", "grexia@gmail.com",
+                "lorenzo@sofka.com", "albert@gmail.com", "dante@gmail.com", "wendy@gmail.com", "dennis@hotmail.com",
+                "camila@outlook.com", "diane@hotmail.com", "camila@gmail.com", "staicy@hotmail.com", "talia@gmail.com",
+                "jinx@sofka.com", "cirilla@gmail.com", "juan@gmail.com", "legend2123@gmail.com", "jaskier@sofka.com",
+                "fenix@outlook.com", "lenin@hotmail.com", "justsad2@gmail.com", "armillia@gmail.com", "yennefer@gmail.com",
+                "dante@gmail.com", "geralt@outlook.com", "keta@gmail.com", "cleopatra@sofka.com", "lilly@sofka.com");
 
 
+        return emails;
+    }
 
 }
